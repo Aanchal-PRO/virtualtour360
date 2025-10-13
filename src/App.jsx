3bc5f8a034a0e64894e3e97af637ba539eebc0f7
MOhdanas001 <anaskslayer786@gmail.com>
2025-10-13 11:08:52 +0530

    Removed large image files

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import PanoramaViewer from './components/PanoramaViewer';
 const panoramas = [
   {
     id: 'scene1',
-    image: './src/assets/7-7.0001.png', // PLACE your real panorama here (or import)
+    image: './src/assets/7-7.0001.webp', // PLACE your real panorama here (or import)
     mask: './src/assets/7-7.0001_Masks.png',
     map: {
       // map "r,g,b" -> building metadata
@@ -17,7 +17,7 @@ const panoramas = [
   },
   {
     id: 'scene2',
-    image: './src/assets/7-2.0001.png',
+    image: './src/assets/7-1.0001.png',
     mask: '',
     map: {
       '10,20,30': { name: 'Other', nextPanorama: 'scene1' },
@@ -25,7 +25,7 @@ const panoramas = [
   },
   {
     id: 'scene3',
-    image: './src/assets/7-1.0001.png',
+    image: './src/assets/7-5.0001.png',
     mask: '',
     map: {
       '10,20,30': { name: 'Other', nextPanorama: 'scene1' },
