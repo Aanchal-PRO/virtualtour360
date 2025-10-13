ee7fe0d1933c6636e40666a466d82cb19d1a653c
MOhdanas001 <anaskslayer786@gmail.com>
2025-10-13 11:24:25 +0530

    changed file location

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,16 @@ import PanoramaViewer from './components/PanoramaViewer';
 const panoramas = [
   {
     id: 'scene1',
-    image: './src/assets/7-7.0001.webp', // PLACE your real panorama here (or import)
-    mask: './src/assets/7-7.0001_Masks.webp',
+    image: '/assets/7-7.0001.webp',       
+    mask: '/assets/7-7.0001_Masks.webp',  
     map: {
-      // map "r,g,b" -> building metadata
-      // Example values; replace with colors from your mask.
       '235,0,212': { name: 'House 1', nextPanorama: 'scene2' },
       '175,188,132': { name: 'House 2', nextPanorama: 'scene3' },
-      // ... add all unique colors used in mask
     },
   },
   {
     id: 'scene2',
-    image: './src/assets/7-1.0001.webp',
+    image: '/assets/7-1.0001.webp',
     mask: '',
     map: {
       '10,20,30': { name: 'Other', nextPanorama: 'scene1' },
@@ -25,14 +22,13 @@ const panoramas = [
   },
   {
     id: 'scene3',
-    image: './src/assets/7-5.0001.webp',
+    image: '/assets/7-5.0001.webp',
     mask: '',
     map: {
       '10,20,30': { name: 'Other', nextPanorama: 'scene1' },
     },
   },
 ];
-
 export default function App() {
   return (
     <div className="h-screen bg-gray-900">
